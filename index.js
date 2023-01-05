e0298771cc36071b693974df361da813b1f1885f
Jerson Fuentes <47575457+jersonfuentes@users.noreply.github.com>
2023-01-05 03:20:46 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,24 @@
+//Resuelve operaciones básicas, es decir, sumas, restas, multiplicación y sustracción.
+function suma(){
+  resultado = 5 + 6
+  console.log(resultado)
+}
+suma()
+
+function resta(){
+  resultado = 5 - 6
+  console.log(resultado)
+}
+resta()
+
+function multiplicacion(){
+  resultado = 5 * 6
+  console.log(resultado)
+}
+multiplicacion()
+
+function division(){
+  resultado = 5 / 6
+  console.log(resultado)
+}
+division()
